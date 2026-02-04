9129aad333b0f5856016272ca507a0f4ed91c30b
Eric Lee <tunglee1228@gmail.com>
2026-02-04 21:33:27 +0800

    update 3

--- Day8/AddFV.java
+++ Day8/AddFV.java
@@ -3,12 +3,13 @@ class AddFv
 	public static void main(String[] args) {
 		//FV.pv=20000;
 		System.out.println(FV.companyName());
-		FV f1=new FV(10000,0.015,2);
-		FV f2=new FV(10000,0.014,3);
-		FV f3=new FV(10000,0.013,4);
-		FV f4=new FV(10000,0.012,5);
-		f1.setR(0.011);
 		FV.setPV(50000);
+		FV f1=new FV(0.015,2);
+		FV f2=new FV(0.014,3);
+		FV f3=new FV(0.013,4);
+		FV f4=new FV(0.012,5);
+		f1.setR(0.011);
+		
 
 		f1.show();
 		f2.show();
