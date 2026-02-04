5c5d573ae52b7042dd90fabb59f57483949fe03a
Eric Lee <tunglee1228@gmail.com>
2026-02-04 21:14:05 +0800

    update 2

--- Day8/AddFV.java
+++ Day8/AddFV.java
@@ -1,10 +1,14 @@
 class AddFv
 {
 	public static void main(String[] args) {
-		Fv f1=new Fv(10000,0.015,2);
-		Fv f2=new Fv(10000,0.014,3);
-		Fv f3=new Fv(10000,0.013,4);
-		Fv f4=new Fv(10000,0.012,5);
+		//FV.pv=20000;
+		System.out.println(FV.companyName());
+		FV f1=new FV(10000,0.015,2);
+		FV f2=new FV(10000,0.014,3);
+		FV f3=new FV(10000,0.013,4);
+		FV f4=new FV(10000,0.012,5);
+		f1.setR(0.011);
+		FV.setPV(50000);
 
 		f1.show();
 		f2.show();
--- Day8/FV.java
+++ Day8/FV.java
@@ -1,20 +1,47 @@
-class Fv
+class FV
 {
 	//field
-	int pv;
-	double r;
-	int n;
-	double fv;
+	private static int pv;
+	private double r;
+	private int n;
+	private double fv;
 
 	//constructors
-	Fv(int pv,double r,int n)
+	FV(int pv,double r,int n)
 	{
 		this.pv=pv;
 		this.r=r;
 		this.n=n;
 		fv=pv*(1+r*n);
 	}
-
-	//f.show();
-
+	FV(double r,int n)
+	{		
+		this.r=r;
+		this.n=n;
+		fv=pv*(1+r*n);
+	}
+	//methods
+	void setR(double r)
+	{
+		if(r>=0)
+		{
+			this.r=r;
+		}
+	}
+	static void setPV(int pv)
+	{
+		FV.pv=pv;
+	}
+	static String companyName()
+	{
+		return "巨匠電腦教育中心";
+	}
+	int call(int x,int y)
+	{
+		return x*y;
+	}
+	void show()
+	{
+		System.out.println("本金:"+pv+"\t利率:"+r+"\t期數:"+n+"\t本利和:"+fv);
+	}
 }
